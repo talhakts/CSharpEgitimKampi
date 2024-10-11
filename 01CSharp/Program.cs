8d61f14cddb03552bcae374c4694fbcacf08454a
talha <talha@TALHA>
2024-10-11 19:30:24 +0300

    Add project files.

---
+++ 01CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace _01CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a= Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            if (a + b > 8)
+            {
+                Console.WriteLine("Toplam 8 den büyük");
+
+
+            }
+            else if (a + b == 8) { Console.WriteLine("Toplam 8"); }
+            else {
+                Console.WriteLine("Toplam 8 den küçük");
+            }
+            Console.ReadKey();
+        }
+    }
+}
